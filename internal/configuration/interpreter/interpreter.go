a4d2e35c55a54d6441341f9f024fc17644c9f05a
Cosmin Tupangiu <cosmin@redhat.com>
2022-07-28 18:33:57 +0200

    remove useless fmt
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>

--- internal/configuration/interpreter/interpreter.go
+++ internal/configuration/interpreter/interpreter.go
@@ -47,8 +47,6 @@ func (i *Interpreter) evaluate(variables map[string]interface{}) (result bool, e
 	a := newAst(variables)
 	v := i.expr.Accept(a)
 
-	fmt.Printf("****** %s\n", i.expr.String())
-
 	if v.typ != typeBool {
 		return false, newEvaluationError(i.expr, "expected bool value. actual '%v'", v.typ)
 	}
